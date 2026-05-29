41506f25e994944ab4f451f5dca70a94c3b41144
BlackAssassinCoder <Ahmadasadi1381.22@gmail.com>
2026-05-29 21:01:17 +0330

    V1.16.1 (FIX page swaping vs scrolling bug)

--- JS/mobile_toch_controll.js
+++ JS/mobile_toch_controll.js
@@ -2,28 +2,47 @@ document.addEventListener('DOMContentLoaded', () => {
     let touchStartX = 0;
     let touchEndX = 0;
 
+    let touchStartY = 0;
+    let touchEndY = 0;
+
     const navLinks = document.querySelectorAll('ul.nav li a');
     const currentPageIndex = Array.from(navLinks).findIndex(link => link.href === window.location.href);
 
     window.addEventListener('touchstart', (e) => {
         touchStartX = e.changedTouches[0].screenX;
+        touchStartY = e.changedTouches[0].screenY;
     });
 
     window.addEventListener('touchend', (e) => {
         touchEndX = e.changedTouches[0].screenX;
+        touchEndY = e.changedTouches[0].screenY;
+
         handleSwipeGesture();
     });
 
     function handleSwipeGesture() {
+        const deltaX = touchEndX - touchStartX;
+        const deltaY = touchEndY - touchStartY;
+
+        const angleRadians = Math.atan2(deltaY, deltaX);
+        let angleDegrees = angleRadians * (180 / Math.PI);
+
+        if (angleDegrees < 0) {
+            angleDegrees += 360;
+        }
+
+        const isRight = angleDegrees >= 315 || angleDegrees < 45;
+        const isLeft = angleDegrees >= 135 && angleDegrees < 225;
+
         const swipeDistance = touchEndX - touchStartX;
         const swipeThreshold = 50;
 
         if (Math.abs(swipeDistance) > swipeThreshold) {
-            if (swipeDistance > 0) {
+            if (isRight & !isLeft) {
                 if (currentPageIndex > 0) {
                     window.location.href = navLinks[currentPageIndex - 1].href;
-                }
-            } else {
+                }       
+            } else if (!isRight & isLeft) {
                 if (currentPageIndex < navLinks.length - 1) {
                     window.location.href = navLinks[currentPageIndex + 1].href;
                 }
